266f94e5c985efaa885ae0f82d5767f6069d82d9
Kema-dev <jeremy.jdn@free.fr>
2021-01-17 13:05:35 -0500

    strcmp fix

--- sources/ft_strcmp.s
+++ sources/ft_strcmp.s
@@ -2,16 +2,17 @@
 
 			section		.text
 ;int		ft_strcmp(const char *s1, const char *s2);
-_ft_strcmp:	xor 		rax, rax
-			xor			rcx, rcx
-			cmp			rsi, 0
+_ft_strcmp:	mov	byte	al, [rdi]
+			mov	byte	bl, [rsi]
+			cmp			al, 0
 			je			zero
-			cmp			rdi, 0
+			cmp			bl, 0
 			je			zero
 			jmp			loop
 
-loop:		mov byte	dl, [rsi + rcx]
-			cmp byte	[rdi + rcx], dl
+loop:		mov	byte	al, [rdi + rcx]
+			mov	byte	bl, [rsi + rcx]
+			cmp byte	al, bl
 			jne			end_loop
 			inc			rcx
 			jmp			loop
