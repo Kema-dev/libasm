87ce1cd85262cdbdb210b5e887dd3ab1c429285a
Kema-dev <jeremy.jdn@free.fr>
2021-01-17 14:11:01 -0500

    correc

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 /*   By: jjourdan <jjourdan@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/16 19:58:55 by jjourdan          #+#    #+#             */
-/*   Updated: 2021/01/17 14:06:00 by jjourdan         ###   ########lyon.fr   */
+/*   Updated: 2021/01/17 14:10:55 by jjourdan         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -120,10 +120,10 @@ int	main(void)
 	printf("\n");
 
 	printf("\n\nTesting write:\n\n");
-	fd1 = open(testfile1, O_CREAT, S_IRWXU);
-	fd2 = open(testfile1, O_CREAT, S_IRWXU);
-	dest1 = read(fd1, dest1, 100);
-	dest2 = read(fd2, dest2, 100);
+	fd1 = open("testfile1", O_CREAT, S_IRWXU);
+	fd2 = open("testfile1", O_CREAT, S_IRWXU);
+	read(fd1, dest1, 100);
+	read(fd2, dest2, 100);
 	while (lencpy[++i])
 	{
 		ft_write(fd1, lencpy[i], 100);
