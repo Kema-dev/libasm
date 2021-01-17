415f907ead2aa38e94d35e55ce9cced64726258b
Kema-dev <jeremy.jdn@free.fr>
2021-01-17 17:14:43 -0500

    ?

--- includes/libasm.h
+++ includes/libasm.h
@@ -6,7 +6,7 @@
 /*   By: jjourdan <jjourdan@student.42lyon.fr>      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/01/16 20:38:57 by jjourdan          #+#    #+#             */
-/*   Updated: 2021/01/17 13:56:35 by jjourdan         ###   ########lyon.fr   */
+/*   Updated: 2021/01/17 17:14:37 by jjourdan         ###   ########lyon.fr   */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,11 +20,11 @@
 #include <stdlib.h>
 #include <fcntl.h>
 
+ssize_t		ft_read(int fd, void *buf, size_t count);
+ssize_t		ft_write(int fd, const void *buf, size_t count);
 size_t		ft_strlen(const char *str);
 char		*ft_strcpy(char *dst, const char *src);
 int			ft_strcmp(const char *s1, const char *s2);
-ssize_t		ft_write(int fd, const void *buf, size_t count);
-ssize_t		ft_read(int fd, void *buf, size_t count);
 char		*ft_strdup(const char *s);
 
 int 		*__errno_location(void);
